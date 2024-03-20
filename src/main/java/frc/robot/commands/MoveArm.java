1c454d0642777b48542e2147ff72e1aec4ceb217
WWLaptop <WWgithub@wiredwizards.org>
2024-03-19 20:20:19 -0400

    bangbang shooter

--- src/main/java/frc/robot/commands/MoveArm.java
+++ src/main/java/frc/robot/commands/MoveArm.java
@@ -3,34 +3,35 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-import frc.robot.subsystems.Arm;
-import edu.wpi.first.wpilibj2.command.Command;
 
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Arm;
 
 public class MoveArm extends Command {
+
   /** Creates a new ControlShooterArm. */
   Arm Arm;
   Double speed;
-  public 
-  MoveArm(Arm Arm, Double speed) {
+
+  public MoveArm(Arm Arm, Double speed) {
     this.Arm = Arm;
     this.speed = speed;
+
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(this.Arm);
   }
-  
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     System.out.println("Move Arm Called");
-
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     Arm.move(speed);
+
     // System.out.println("Moving Arm Spd:"+speed);
 
     // Test Low and high limits then implement limit switch logic
@@ -52,6 +53,7 @@ public class MoveArm extends Command {
     //   }
     // }
   }
+
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
