2c5799e3fd0ac15a9c278a3512e322f339ab59fc
Pekay <github@maxlingenfelter.com>
2024-03-22 15:25:22 -0400

    Create autoShoot.java

---
+++ src/main/java/frc/robot/commands/AutoRoutines/autoShoot.java
@@ -0,0 +1,46 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.AutoRoutines;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.CommandConstants;
+import frc.robot.commands.CalculateArmAngle;
+import frc.robot.commands.DoNothing;
+import frc.robot.commands.PIDMoveArm;
+import frc.robot.commands.PointToSpeaker2;
+import frc.robot.commands.RunShooter;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.ArmProfiledPID;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class autoShoot extends SequentialCommandGroup {
+
+  /** Creates a new PlaceAndStation. */
+  public autoShoot(
+    Limelight limelight,
+    SwerveSubsystem swerve,
+    Arm arm,
+    ArmProfiledPID armProfiledPID,
+    Intake intake,
+    Shooter shooter
+  ) {
+    addCommands(
+      new ParallelDeadlineGroup(
+        new ParallelDeadlineGroup(
+          new CalculateArmAngle(arm, armProfiledPID, limelight),
+          new PointToSpeaker2(limelight, swerve)
+        ),
+        new RunShooter(shooter, intake, () -> 1.0, false, false, false)
+      ),
+      new RunShooter(shooter, intake, () -> 1.0, false, true, true)
+    );
+  }
+}
