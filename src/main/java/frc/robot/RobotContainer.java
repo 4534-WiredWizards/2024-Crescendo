c21dae9f4b6ec22e6652e6322d7e0a16182b8705
Pekay <github@maxlingenfelter.com>
2024-04-02 17:50:51 -0400

    Focus Shoot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -21,8 +20,8 @@ import frc.robot.Constants.InputDevices;
 import frc.robot.Constants.OIConstants;
 import frc.robot.commands.AutoRoutines.autoShoot;
 import frc.robot.commands.CalculateArmAngle;
+import frc.robot.commands.FocusShoot;
 import frc.robot.commands.MoveArm;
-import frc.robot.commands.PIDAtGoal;
 import frc.robot.commands.PIDMoveArm;
 import frc.robot.commands.PointToSpeaker;
 import frc.robot.commands.RampUpShooter;
@@ -31,13 +30,10 @@ import frc.robot.commands.RunShooter;
 import frc.robot.commands.Shoot;
 import frc.robot.commands.ShootOnPID;
 import frc.robot.commands.SwerveJoystickCmd;
-import frc.robot.commands.runClimb;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.ArmProfiledPID;
 import frc.robot.subsystems.AutoChooser;
-import frc.robot.subsystems.Climb;
 import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Lights;
 import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.SwerveSubsystem;
@@ -194,7 +190,7 @@ public class RobotContainer {
 
     autoChooserTwo = AutoBuilder.buildAutoChooser();
     // Clear Existing entries
-    SmartDashboard.putData("Path Planner #1", autoChooserTwo);
+    SmartDashboard.putData("Path Planner", autoChooserTwo);
 
     configureButtonBindings();
   }
@@ -222,6 +218,20 @@ public class RobotContainer {
           .until(() ->
             operatorJoystick.getRawButtonPressed(InputDevices.btn_start) // Btn to cancel all autoshoot commands
           )
+      );
+
+    // Make a new command similar to the above but it requires the button be held down to run
+    new JoystickButton(operatorJoystick, InputDevices.btn_start)
+      .whileTrue(
+        new FocusShoot(
+          limelight,
+          swerve,
+          arm,
+          armProfiledPID,
+          () -> -driverJoystick.getRawAxis(OIConstants.kDriverYAxis),
+          () -> -driverJoystick.getRawAxis(OIConstants.kDriverXAxis),
+          () -> -driverJoystick.getRawAxis(OIConstants.kDriverThrottleAxis)
+        )
       );
 
     // ----------------------- INTAKE & Shooter COMMANDS ---------------------------------
---
+++ src/main/java/frc/robot/commands/FocusShoot.java
@@ -0,0 +1,133 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.ArmProfiledPID;
+import frc.robot.subsystems.Limelight;
+import frc.robot.subsystems.SwerveSubsystem;
+import java.util.function.Supplier;
+
+public class FocusShoot extends Command {
+
+  //Command that works similar to PointToSpeaker.java, but allows continued input of x and y values the robot while its rotating to the target
+  //Also moves the arm to the target height using   new CalculateArmAngle(arm, armProfiledPID, limelight, false)
+  private final Supplier<Double> xSpdFunction, ySpdFunction, throttle;
+  private double throttleadjusted;
+  Limelight limelight;
+  SwerveSubsystem swerve;
+  CalculateArmAngle calculateArmAngle;
+  ArmProfiledPID armProfiledPID;
+  Arm arm;
+  PIDController PIDController;
+  Double distance;
+  private final SlewRateLimiter xLimiter, yLimiter;
+
+  /** Creates a new PointToSpeaker2. */
+  public FocusShoot(
+    Limelight limelight,
+    SwerveSubsystem swerve,
+    Arm arm,
+    ArmProfiledPID armProfiledPID,
+    Supplier<Double> xSpdFunction,
+    Supplier<Double> ySpdFunction,
+    Supplier<Double> throttle
+  ) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.limelight = limelight;
+    this.swerve = swerve;
+    this.xSpdFunction = xSpdFunction;
+    this.ySpdFunction = ySpdFunction;
+    this.throttle = throttle;
+    this.xLimiter =
+      new SlewRateLimiter(
+        DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond
+      );
+    this.yLimiter =
+      new SlewRateLimiter(
+        DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond
+      );
+
+    // Constant values
+    calculateArmAngle =
+      new CalculateArmAngle(arm, armProfiledPID, limelight, false);
+    PIDController = new PIDController(0.3, .75, 0.02);
+    PIDController.setTolerance(2, 1);
+    addRequirements(swerve);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    System.out.println(
+      "Command '" +
+      this.getName() +
+      "' initialized at " +
+      Timer.getFPGATimestamp()
+    );
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    distance = swerve.getHeading() - (limelight.gettx() + 4);
+
+    // 1. Get real-time joystick inputs
+    throttleadjusted = throttle.get() * -.25 + .75;
+    double xSpeed = xSpdFunction.get() * throttleadjusted;
+    double ySpeed = ySpdFunction.get() * throttleadjusted;
+    double turningSpeed = PIDController.calculate(
+      swerve.getHeading(),
+      distance
+    );
+    // 2. Apply deadband
+    xSpeed = Math.abs(xSpeed) > OIConstants.kDeadband ? xSpeed : 0.0;
+    ySpeed = Math.abs(ySpeed) > OIConstants.kDeadband ? ySpeed : 0.0;
+
+    // 3. Make the driving smoother
+    xSpeed =
+      xLimiter.calculate(xSpeed) *
+      DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+    ySpeed =
+      yLimiter.calculate(ySpeed) *
+      DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+
+    // 4. Construct desired chassis speeds && move arm to target height
+    calculateArmAngle.execute(); // Move arm to target height
+    ChassisSpeeds chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+      xSpeed,
+      ySpeed,
+      turningSpeed * .7,
+      swerve.getRotation2d()
+    );
+    SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
+      chassisSpeeds
+    );
+    swerve.setModuleStates(moduleStates);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    swerve.stopModules();
+    System.out.println(
+      "Command '" + this.getName() + "' ended at " + Timer.getFPGATimestamp()
+    );
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
